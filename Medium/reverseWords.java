4938d02887d423b877ed52f6dfb632434934022b
chelseaY119 <ayang106@my.bcit.ca>
2023-09-20 12:25:18 -0700

    update answer for q151

---
+++ Medium/reverseWords.java
@@ -0,0 +1,39 @@
+package Medium;
+
+class Solution {
+
+    public String reverseWords(String s) {
+
+        String trimmedString = s.trim();
+        String[] origin = trimmedString.split("\\s+");
+        String[] newStrings = new String[origin.length];
+
+        int left = 0;
+        int right = newStrings.length - 1;
+
+        while (left <= right) {
+            String temp = origin[left];
+            newStrings[right] = temp;
+            newStrings[left] = origin[right];
+            left++;
+            right--;
+
+            if (left == right) {
+                newStrings[right] = origin[right];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < origin.length; i++) {
+            sb.append(newStrings[i]);
+            if (i != (origin.length - 1)) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+}
