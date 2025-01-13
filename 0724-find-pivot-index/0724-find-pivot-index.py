87872506c6840b80344e11f083483ba329eaff8a
chels.y119 <ayang106@my.bcit.ca>
2025-01-13 15:10:27 -0800

    Time: 1 ms (92.73%), Space: 13.4 MB (18.28%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        totalsum = sum(nums) # total sum
+        leftsum = 0
+
+        for i in range(len(nums)):
+            # leftsum vs rightsum (totalsum - leftsum - current number)
+            if leftsum == totalsum - leftsum - nums[i]:
+                return i
+            leftsum += nums[i]
+
+        return -1
+
+        
+
+
+
