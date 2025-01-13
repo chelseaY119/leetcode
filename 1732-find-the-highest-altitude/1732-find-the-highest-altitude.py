e5a7d61b4e8d863f56c4f948d86879f35fb2c2c2
chels.y119 <ayang106@my.bcit.ca>
2025-01-13 14:40:00 -0800

    Time: 0 ms (100%), Space: 12.4 MB (40.9%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+
+        highest_alt = 0
+        current_alt = 0
+        n = len(gain)
+
+        for i in range(n):
+            current_alt = current_alt + gain[i]
+            highest_alt = max (highest_alt, current_alt)
+        
+        return highest_alt
+
+        
