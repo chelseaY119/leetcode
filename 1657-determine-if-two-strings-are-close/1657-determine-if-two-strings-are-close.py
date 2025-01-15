57f7da333464bdff6d2bfc8c592865adf2001b06
chels.y119 <ayang106@my.bcit.ca>
2025-01-14 18:08:34 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def closeStrings(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        
+        word1_map = {}
+        word2_map = {}
+
+        # return false if the length of two words is different 
+        if len(word1) != len(word2):
+            return False
+
+        for char in word1:
+            word1_map[char] = word1_map.get(char, 0) + 1
+        
+        for char in word2:
+            word2_map[char] = word2_map.get(char, 0) + 1
+        
+        if set(word1_map.keys()) != set(word2_map.keys()):
+            return False
+        
+        return True
