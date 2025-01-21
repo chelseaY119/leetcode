a5b381463e254853b4f99ab25b7a0d7bcd88f2cd
chels.y119 <ayang106@my.bcit.ca>
2025-01-21 17:28:21 -0500

    Time: 279 ms (52.29%), Space: 19.4 MB (6.02%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -4,14 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        n = len(s) # length of the original string, used for loop
         stack = [] # stack to store the output string
-
         for char in s:
             if char != '*':
                 stack.append(char)
             else:
                 stack.pop()
-        
         result = "".join(map(str, stack))
         return result
