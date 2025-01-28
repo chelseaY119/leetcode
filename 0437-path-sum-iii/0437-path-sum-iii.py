8d92a0ac0f7fc2f9fd1469df3341cb7a1e5840ee
chels.y119 <ayang106@my.bcit.ca>
2025-01-28 02:09:21 -0500

    Time: 795 ms (22.24%), Space: 14 MB (27.25%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: int
+        """
+
+        def count_path_from_node(node, targetSum):
+            if not node:
+                return 0
+            
+            count = 1 if node.val == targetSum else 0
+            
+            count += count_path_from_node(node.left, targetSum - node.val)
+            count += count_path_from_node(node.right, targetSum - node.val)
+            
+            return count
+        
+        if not root:
+            return 0
+        
+        count = count_path_from_node(root, targetSum)
+        count += self.pathSum(root.left, targetSum)
+        count += self.pathSum(root.right, targetSum)
+        
+        return count
+            
+
+
+
+
+
+
+
+
+
+        
